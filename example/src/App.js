236dce6fd36dfdaea58cf00988531f678edc1cbf
Long Nguyen <nclong87@gmail.com>
2019-11-28 15:24:18 +0800

    Add PDF reader component

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react'
 
-import ExampleComponent from 'ngager-pdfreader'
+import PDF from 'ngager-pdfreader'
 
 export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      open: true
+    }
+  }
   render () {
+    if (this.state.open === false) {
+      return null;
+    }
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <PDF
+          popup
+          src="https://file-examples.com/wp-content/uploads/2017/10/file-example_PDF_500_kB.pdf"
+          downloadable
+          onRequestClose={() => this.setState({ open: false })}
+          onLastPage={() => console.log('onLastPage')}
+        />
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,22 +1,138 @@
-import React, { Component } from 'react'
+import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 
 import styles from './styles.css'
 
-export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string
+class PDF extends PureComponent {
+  constructor(props) {
+    super(props)
+    this.state = {
+      currentPage: 1,
+      totalPages: 0
+    }
+    this.handleOnReceived = this.handleOnReceived.bind(this)
+    this.handlePressEscape = this.handlePressEscape.bind(this)
+  }
+
+  componentDidMount() {
+    window.addEventListener('message', this.handleOnReceived)
+    document.addEventListener('keydown', this.handlePressEscape, false)
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { currentPage, totalPages } = this.state
+    if (prevState.totalPages !== totalPages || prevState.currentPage !== currentPage) {
+      if (currentPage === totalPages) {
+        this.props.onLastPage()
+      }
+    }
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('message', this.handleOnReceived)
+    document.removeEventListener('keydown', this.handlePressEscape, false)
+  }
+
+  handlePressEscape(e) {
+    if (e.keyCode === 27) {
+      if (typeof e.preventDefault === 'function') {
+        e.preventDefault()
+      }
+      if (typeof e.stopPropagation === 'function') {
+        e.stopPropagation()
+      }
+      this.props.onRequestClose()
+    }
+  }
+
+  handleOnReceived(event) {
+    const host = this.props.viewer.host
+    if (event.origin !== host) {
+      return
+    }
+    if (event.data && event.data.type) {
+      // console.log('Received Message : ', event)
+      if (!this.iframe) {
+        return
+      }
+      switch (event.data.type) {
+        case 'ready': {
+          const resource = {
+            src: this.props.src,
+            popup: this.props.popup,
+            downloadable: this.props.downloadable
+          }
+          this.iframe.contentWindow.postMessage(resource, host)
+          break
+        }
+        case 'close':
+          this.props.onRequestClose()
+          break
+        case 'pagesloaded': {
+          const { pagesCount } = event.data.data
+          this.setState({ totalPages: pagesCount })
+          break
+        }
+        case 'pagechanging': {
+          const { pageNumber } = event.data.data
+          this.setState({ currentPage: pageNumber })
+          break
+        }
+        default:
+          break
+      }
+    }
+  }
+
+  renderIframe() {
+    const { host, path } = this.props.viewer
+    return (
+      <iframe
+        ref={el => {
+          this.iframe = el
+        }}
+        frameBorder='0'
+        allowFullScreen
+        style={{ width: '100%', height: '100%' }}
+        title='PDF Reader'
+        src={`${host}${path}`}
+      />
+    )
   }
 
   render() {
-    const {
-      text
-    } = this.props
-
-    return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
-    )
+    if (this.props.popup) {
+      return (
+        <div className={styles.popup}>
+          {this.renderIframe()}
+        </div>
+      )
+    }
+    return this.renderIframe()
   }
 }
+
+PDF.propTypes = {
+  viewer: PropTypes.instanceOf(Object),
+  popup: PropTypes.bool,
+  downloadable: PropTypes.bool,
+  src: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.instanceOf(Object)
+  ]).isRequired,
+  onLastPage: PropTypes.func,
+  onRequestClose: PropTypes.func
+}
+
+PDF.defaultProps = {
+  viewer: {
+    host: 'https://nclong87.github.io',
+    path: '/web/viewer.html'
+  },
+  popup: false,
+  downloadable: false,
+  onRequestClose: () => null,
+  onLastPage: () => null
+}
+
+export default PDF
